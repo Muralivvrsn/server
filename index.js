c3496b4333be64f2985909826d18a327b908e8ea
Murali <muralivvrsn75683@gmail.com>
2023-04-19 18:28:26 +0530

    port

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const app = express();
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser")
+const port = process.env.PORT || 4000
 
 try{
     mongoose.connect("mongodb+srv://murali:murali1889@cluster0.ev7pbcx.mongodb.net/?retryWrites=true&w=majority");
@@ -22,6 +23,6 @@ app.get('/',(req,res)=>{
     res.send("Hello World")
 });
 
-app.listen(4000,()=>{
+app.listen(port,()=>{
     console.log("Listening to port 4000")
 })
